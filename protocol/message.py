e10e9390687023f0c327ab80571c12da6eefbbc0
Liad Paz <liadpaz100@gmail.com>
2020-12-29 11:36:14 +0200

    Added message

---
+++ protocol/message.py
@@ -0,0 +1,24 @@
+"""
+Name:       message.py
+
+Purpose:    This file contains the Message class.
+"""
+from abc import ABCMeta, abstractmethod
+
+
+class Message(metaclass=ABCMeta):
+
+    @abstractmethod
+    @property
+    def type(self) -> int:
+        """
+        :return: The message type.
+        """
+
+    @abstractmethod
+    def pack_message(self) -> bytes:
+        """
+        Use this method to prepare the message for sending as a packet.
+
+        :return: The message as a packet data.
+        """
